d269ae4c92b83aeaebc18052b8e31489856d2d66
장재영 <jangjaeyeong@jangjaeyeong-ui-noteubug.local>
2024-11-09 23:29:52 +0900

    feat: 증정 상품 할인 금액 계산
    증정 삼품의 할인 금액을
    계산하기 위해서
    기능을 구현한다.

---
+++ src/main/java/store/domain/util/discount/EventDiscount.java
@@ -0,0 +1,29 @@
+package store.domain.util.discount;
+
+import store.domain.Items;
+import store.domain.util.ItemsMap;
+
+import java.util.Map;
+
+public class EventDiscount {
+
+    private EventDiscount(){}
+
+    public static int discount(Map<String, Integer> giftProduct) {
+        if (giftProduct == null) {
+            return 0;
+        }
+        return calculateDiscountPrice(giftProduct);
+    }
+
+    private static int calculateDiscountPrice(Map<String, Integer> giftProduct) {
+        int discountPrice = 0;
+        Map<String, String> items = ItemsMap.create();
+        for (String product : giftProduct.keySet()) {
+            Items item = Items.valueOf(items.get(product));
+            int totalPrice = item.totalPrice(giftProduct.get(product));
+            discountPrice -= totalPrice;
+        }
+        return discountPrice;
+    }
+}
