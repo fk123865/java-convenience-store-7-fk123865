074a8d8b64b3f12633874ac2791308acc00853bb
장재영 <jangjaeyeong@192.168.nate.com>
2024-11-09 19:46:41 +0900

    test: Product 파일 읽기 테스트
    Products 파일을 읽기 위해서
    테스트 코드를 작성한다.

---
+++ src/test/java/store/domain/file/ProductDataTest.java
@@ -0,0 +1,24 @@
+package store.domain.file;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import store.domain.Product;
+import store.domain.Promotion;
+import store.file.ProductData;
+import store.file.PromotionData;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Map;
+
+public class ProductDataTest {
+
+    @Test
+    void createProductDataTest() throws FileNotFoundException {
+        Map<String, Promotion> promotionData = PromotionData.create("src/main/resources/promotions.md");
+
+        List<Product> products = ProductData.create("src/main/resources/products.md", promotionData);
+
+        Assertions.assertThat(products).hasSize(16);
+    }
+}
