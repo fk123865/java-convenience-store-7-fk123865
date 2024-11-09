b40e579f8336e9e163c84e475ca0b6f95fd897ac
장재영 <jangjaeyeong@192.168.nate.com>
2024-11-09 19:28:13 +0900

    test: Promotion 파일 읽기 테스트
    Promotion 파일을 읽기 위해서
    테스트를 작성한다.

---
+++ src/test/java/store/file/PromotionDataTest.java
@@ -0,0 +1,24 @@
+package store.file;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import store.domain.Promotion;
+import store.file.PromotionData;
+
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PromotionDataTest {
+
+    @Test
+    @DisplayName("promotions.md 파일을 가지고 Promotion 객체를 보관하는 리스트를 생성한다.")
+    void createPromotionDataTest() throws FileNotFoundException {
+        // when
+        Map<String, Promotion> promotions = PromotionData.create("src/main/resources/promotions.md");
+
+        //then
+        assertThat(promotions).hasSize(3);
+    }
+}
