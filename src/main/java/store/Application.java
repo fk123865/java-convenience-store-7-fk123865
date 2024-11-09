dbdff1573fbe4989ad7a7baa97e9aabc11ea8441
장재영 <jangjaeyeong@192.168.nate.com>
2024-11-09 20:42:08 +0900

    feat: 파일 작성 기능 추가
    남은 재고를 다시 파일로 저장하기 위해서
    파일 작성 기능을 추가한다.

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,11 @@
 package store;
 
+import store.controlller.StoreController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        StoreController controller = new StoreController();
+        controller.run();
     }
 }
---
+++ src/main/java/store/domain/util/file/WriteProductData.java
@@ -0,0 +1,28 @@
+package store.domain.util.file;
+
+import store.domain.Product;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class WriteProductData {
+
+    private WriteProductData() {}
+
+    public static void write(List<Product> products, String filePath) throws IOException {
+        File file = new File(filePath);
+
+        FileWriter fw = new FileWriter(file);
+        PrintWriter pw = new PrintWriter(fw);
+
+        pw.println("name,price,quantity,promotion");
+        for (Product product : products) {
+            pw.println(product.writeString());
+        }
+        pw.close();
+    }
+
+}
