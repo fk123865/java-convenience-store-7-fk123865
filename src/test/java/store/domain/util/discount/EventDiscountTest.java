4c9367b3fd95cad7b17baf8d62bacebfd36dc98e
장재영 <jangjaeyeong@jangjaeyeong-ui-noteubug.local>
2024-11-09 23:29:02 +0900

    test: 증정 상품 할인 금액 계산 테스트
    증정 상품이 있을 때
    할인 금액을 계산하기 위해서
    테스트 코드를 작성한다.

---
+++ src/test/java/store/domain/util/discount/EventDiscountTest.java
@@ -0,0 +1,38 @@
+package store.domain.util.discount;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventDiscountTest {
+
+    @Test
+    @DisplayName("증점품이 1개 일 때 할인 금액을 계산한다.")
+    void discountTest1() {
+        Map<String, Integer> giftProduct = new HashMap<>();
+        giftProduct.put("콜라", 1);
+
+        int discountPrice = EventDiscount.discount(giftProduct);
+
+        assertThat(discountPrice).isEqualTo(-1000);
+    }
+
+    @Test
+    @DisplayName("증점품이 2개 일 때 할인 금액을 계산한다.")
+    void discountTest2() {
+        Map<String, Integer> giftProduct = new HashMap<>();
+        giftProduct.put("콜라", 1);
+        giftProduct.put("사이다", 2);
+
+        int discountPrice = EventDiscount.discount(giftProduct);
+
+        assertThat(discountPrice).isEqualTo(-3000);
+    }
+
+}
