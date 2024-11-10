9494ff950fa8a7b59895a8a3c202133f45df76ad
장재영 <jangjaeyeong@jangjaeyeong-ui-noteubug.local>
2024-11-10 23:24:00 +0900

    feat: Controller 추가
    전체적인 로직을 수행하기 위해서
    Controller 객체를 추가하여
    전체 비지니스 로직을 수행한다.

---
+++ src/main/java/store/controlller/StoreController.java
@@ -0,0 +1,64 @@
+package store.controlller;
+
+import store.model.Store;
+import store.validator.YesOrNoValidator;
+import store.view.InputView;
+import store.view.OutputView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class StoreController {
+
+    private final InputView input;
+    private final OutputView output;
+
+    public StoreController() {
+        input = new InputView();
+        output = new OutputView();
+    }
+
+    public void run() {
+        while (true) {
+            Store store = execute();
+            if (retry().equals("n")) {
+                store.returnProduct();
+                return;
+            }
+        }
+    }
+
+    private String retry() {
+        while (true) {
+            try {
+                String userInput = input.readRetry();
+                return YesOrNoValidator.validate(userInput);
+            } catch (IllegalArgumentException e) {
+                output.printExceptionMessage(e.getMessage());
+            }
+        }
+    }
+
+    private Store execute() {
+        Store store = init();
+        output.printProducts(store.getProducts());
+        while (true) {
+            try {
+                store.sendOrder(input.readOrder());
+                String result = store.calculateTotal();
+                output.printOrderReceipt(result);
+                return store;
+            } catch (IllegalArgumentException e) {
+                output.printExceptionMessage(e.getMessage());
+            }
+        }
+    }
+
+    private Store init() {
+        try {
+            return new Store();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
