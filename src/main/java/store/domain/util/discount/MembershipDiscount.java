34541293ba013313e45e78068372d79272efdedb
장재영 <jangjaeyeong@jangjaeyeong-ui-noteubug.local>
2024-11-10 23:10:22 +0900

    feat: 멤버쉽 할인 기능 추가
    멤버십 할인을 받기 위해서
    일반 상품의 가격을 받아
    그 가격의 30프로를 반환한다.

---
+++ src/main/java/store/domain/util/discount/MembershipDiscount.java
@@ -0,0 +1,32 @@
+package store.domain.util.discount;
+
+import store.validator.YesOrNoValidator;
+import store.view.InputView;
+import store.view.OutputView;
+
+public class MembershipDiscount {
+
+    private MembershipDiscount() {}
+
+    public static int discount(int totalPrice) {
+        String input = discountInput();
+        if (input.equals("n") || totalPrice == 0) {
+            return 0;
+        }
+        double discountPrice = totalPrice * 0.3;
+        return (int) discountPrice;
+    }
+
+    private static String discountInput() {
+        InputView input = new InputView();
+        OutputView output = new OutputView();
+        while (true) {
+            try {
+                String userInput = input.readMembership();
+                return YesOrNoValidator.validate(userInput);
+            } catch (IllegalArgumentException e) {
+                output.printExceptionMessage(e.getMessage());
+            }
+        }
+    }
+}
