a832f89dedbf19fde338789f10e278099754e5cc
장재영 <jangjaeyeong@jangjaeyeong-ui-noteubug.local>
2024-11-10 22:34:36 +0900

    test: 테스트 수정
    Product 객체의 역할을 문서화하기 위해서
    테스트 코드를 수정한다.

--- src/test/java/store/domain/ProductTest.java
+++ src/test/java/store/domain/ProductTest.java
@@ -6,6 +6,8 @@ import store.domain.Product;
 import store.error.ErrorMessage;
 import store.validator.OrderValidator;
 
+import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,26 +16,57 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class ProductTest {
 
     @Test
-    @DisplayName("각 상품의 재고 수량을 고려하여 결제 가능하면 현재 상품의 재고에서 차감하여 수량을 관리한다.")
-    void productsTest2() {
+    @DisplayName("프로모션 재고인지 확인한다.")
+    void isPromotionTest() {
         Product product = getProduct();
-        Map<String, Integer> order = OrderValidator.validate("[콜라-10]");
 
-        int quantity = product.availableForBuy(order);
+        boolean result = product.isPromotion();
 
-        assertThat(quantity).isEqualTo(0);
+        assertThat(result).isTrue();
     }
 
     @Test
-    @DisplayName("각 상품의 재고 수량을 고려하여 결제가 불가능하면 예외를 발생시킨다.")
-    void productsTest3() {
+    @DisplayName("계산이 가능한지 여부를 테스트한다.")
+    void paymentAvailableTest() {
         Product product = getProduct();
-        Map<String, Integer> order = OrderValidator.validate("[콜라-100]");
 
-        assertThatThrownBy(() -> product.availableForBuy(order))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.IS_EXCESS_QUANTITY.toString());
+        boolean result = product.paymentAvailable(3);
+
+        assertThat(result).isTrue();
     }
+
+    @Test
+    @DisplayName("프로모션 날짜에 해당하는지 테스트한다.")
+    void checkDateTest() {
+        Product product = getProduct();
+
+        boolean result = product.checkDate(LocalDate.of(2025, 1, 1));
+
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("일반 상품의 재고를 사용하면 일반 상품의 결제 금액을 반환한다.")
+    void generalAvailableTest() {
+        Product product = getProduct();
+
+        int totalPrice = product.generalAvailableForBuy("콜라", 3);
+
+        assertThat(totalPrice).isEqualTo(3000);
+    }
+
+    @Test
+    @DisplayName("프로모션 재고를 사용하면 증정 상품에 등록된다.")
+    void promotionAvailableTest() {
+        Product product = getProduct();
+        Map<String, Integer> giftProducts = new HashMap<>();
+        Map<String, Integer> order = OrderValidator.validate("[콜라-3]");
+
+        product.promotionAvailableForBuy("콜라", 3, giftProducts, order);
+
+        assertThat(giftProducts.get("콜라")).isEqualTo(1);
+    }
+
 
     private Product getProduct() {
         String name = "콜라";
