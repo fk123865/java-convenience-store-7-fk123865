67d2d2e37a9ece3e8dbc0790ebf066daaff36e31
장재영 <jangjaeyeong@jangjaeyeong-ui-noteubug.local>
2024-11-10 23:22:52 +0900

    feat: Model 역할인 Store 객체 추가
    주문 내역을 통해 재고를 수정하고
    파일 입출력을 하기 위해서
    모델 역할인 Store 객체를 구현하여 추가한다.

---
+++ src/main/java/store/model/Store.java
@@ -0,0 +1,58 @@
+package store.model;
+
+import store.domain.InventoryManagement;
+import store.domain.Product;
+import store.domain.Promotion;
+import store.domain.util.file.ProductData;
+import store.domain.util.file.PromotionData;
+import store.domain.util.file.WriteProductData;
+import store.validator.OrderValidator;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class Store {
+
+    private List<Product> products;
+    private final Map<String, Promotion> promotions;
+    private Map<String, Integer> order;
+
+    public Store() throws FileNotFoundException {
+        promotions = PromotionData.create("src/main/resources/promotions.md");
+        products = ProductData.create("src/main/resources/products.md", promotions);
+    }
+
+    public void sendOrder(String order) {
+        this.order = OrderValidator.validate(order);
+    }
+
+    public String calculateTotal() {
+        InventoryManagement inventory = new InventoryManagement(products, order);
+        inventory.calculate();
+        try {
+            WriteProductData.write(products, "src/main/resources/products.md");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return inventory.writeReceipt();
+    }
+
+    public void returnProduct() {
+        try {
+            products = ProductData.create("src/main/resources/temp.md", promotions);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            WriteProductData.write(products, "src/main/resources/products.md");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+}
